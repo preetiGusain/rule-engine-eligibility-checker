ab7fa1468e8d4c06874f4c6efaa3d103062432fb
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-10-22 18:43:39 +0530

    Initial commit

---
+++ src/main/java/com/ruleflex/ast/AstApplication.java
@@ -0,0 +1,13 @@
+package com.ruleflex.ast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruleflex/ast/AstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruleflex.ast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
